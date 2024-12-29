9373bce3d3c67a14983c740f9320c26a221e5d94
ken <kenwebworkwatarai@gmail.com>
2024-12-29 16:24:02 +0900

    set up router and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import HomePage from './pages/HomePage'
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+)
 
 function App() {
-  return (<div>App</div>)
+  return <RouterProvider router={router} />
 }
 export default App
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,5 @@
+function MainLayout(){
+  return <div>Hello, World!</div>;
+};
+
+export default MainLayout;
