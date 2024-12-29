76864b471968b966a85282f152c13a26a888ff7c
ken <kenwebworkwatarai@gmail.com>
2024-12-29 17:10:42 +0900

    update header and footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,17 +1,36 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useEffect, useRef, useState } from "react";
 
 function MainLayout(){
+  
+  const headerRef = useRef<HTMLElement | null>(null);
+  const footerRef = useRef<HTMLElement | null>(null);
+
+  const [headerHeight, setHeaderHeight] = useState<number | null>(null);
+  const [footerHeight, setFooterHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (headerRef.current) {
+      setHeaderHeight(headerRef.current.getBoundingClientRect().height);
+    }
+  }, []);
+  useEffect(() => {
+    if (footerRef.current) {
+      setFooterHeight(footerRef.current.getBoundingClientRect().height);
+    }
+  }, []);
+
   return (
     <>
-      <header className="flex justify-between bg-blue-600 py-2 px-5">
+      <header ref={headerRef} className="flex justify-between bg-blue-600 py-2 px-5">
         <Header />
       </header>
-      <main className="bg-blue-300 px-5">
+      <main className="bg-blue-200 px-5" style={{ minHeight: `calc(100vh - ${headerHeight}px - ${footerHeight}px)` }} >
         <Outlet />
       </main>
-      <footer className="bg-blue-300">
+      <footer ref={footerRef} className="bg-blue-200">
         <Footer />
       </footer>
     </>
