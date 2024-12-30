e6f75067e59d61aa8c6f6fcb4802f1dc9b520dd1
ken <kenwebworkwatarai@gmail.com>
2024-12-30 21:57:15 +0900

    update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 function HomePage(){
   return (
     <>
-    <div className="wrapper m-auto pt-10 flex flex-col justify-center items-center">
-      <div className="circleArea">
-        <div className="circle">circle</div>
+    <div className="wrapper m-auto pt-5 flex flex-col justify-center items-center">
+      <div className="w-11/12 max-h-80 mb-7 aspect-square">
+        <div className="visualTimer w-full h-full rounded-full"></div>
       </div>
       <div className="w-full text-center flex justify-center items-center py-2 px-4 rounded-md backdrop-opacity-5 backdrop-invert bg-white/60">
         <div className="flex-1 text-5xl font-bold h-auto">25:00</div>
         <div className="flex-1">
-          <select name="setting" id="setting" className="mb-4 text-lg bg-transparent">
+          <select name="setting" id="setting" className="mb-3 text-lg bg-transparent">
             <option value="5">5 min</option>
             <option value="10">10 min</option>
             <option value="15">15 min</option>
@@ -25,20 +25,10 @@ function HomePage(){
         .wrapper {
           width: 85vw;
           max-width: 350px;
-          border: solid 1px black;
+          /* border: solid 1px black; */
         }
-        .circleArea {
-          height: 85vw;
-          max-height: 350px;
-          width: 100%;
-          margin-bottom: .5rem;
-          border: solid 1px black;
-        }
-        .circle {
-          width: 100%;
-          height: 100%;
-          border-radius: 50%;
-          border: solid 1px black;
+        .visualTimer {
+          background: conic-gradient(#eaeaea 195deg, #061298 195deg 18deg);
         }
       `}</style>
     </>
