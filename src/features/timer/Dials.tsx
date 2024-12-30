a88ed07a74e482649e5d12691227a849ef003347
ken <kenwebworkwatarai@gmail.com>
2024-12-30 23:19:23 +0900

    update home page

---
+++ src/features/timer/Dials.tsx
@@ -0,0 +1,33 @@
+function Dials(){
+
+  interface dials {
+    value: number;
+    top: string;
+    left: string;
+  }
+
+  const timeDials: dials[] = [
+    {value: 0, top: '-15px', left: '50%'},
+    {value: 5, top: '2.5%', left: '23%'},
+    {value: 10, top: '22%', left: '1.5%'},
+    {value: 15, top: '50%', left: '-19px'},
+    {value: 20, top: '78%', left: '1.5%'},
+    {value: 25, top: '98%', left: '23%'},
+  ] 
+
+  return (
+    <>
+      {timeDials.map((dial) => (
+      <span
+        key={dial.value}
+        className="absolute -translate-x-1/2 -translate-y-1/2 text-lg font-bold"
+        style={{top: dial.top, left: dial.left}}
+      >
+        {dial.value}
+      </span>
+      ))}
+    </>
+  );
+};
+
+export default Dials;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from "react";
+import Dials from "../features/timer/Dials";
+
 function HomePage(){
+
+  const [remainingTime, setRemainingTime] = useState<number>(25 * 60);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isRunning && remainingTime > 0) {
+      timerId = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (remainingTime === 0) {
+      setIsRunning(false);
+    }
+
+    return () => {
+      if (timerId) clearInterval(timerId);
+    };
+  }, [isRunning, remainingTime]);
+
+  const minutes: number = Math.floor(remainingTime / 60);
+  const seconds: number = remainingTime % 60;
+
+  const handleClick = ():void => {
+    setIsRunning(!isRunning);
+  }
+
   return (
     <>
     <div className="wrapper m-auto pt-5 flex flex-col justify-center items-center">
-      <div className="w-11/12 max-h-80 mb-7 aspect-square">
-        <div className="visualTimer w-full h-full rounded-full"></div>
+      <div className="relative w-11/12 max-h-80 mb-7 aspect-square">
+        <Dials />
+        <div 
+          className="visualTimer w-full h-full rounded-full"
+          style={{background: 'conic-gradient(#eaeaea 210deg, #061298 210deg)'}}
+        ></div>
       </div>
       <div className="w-full text-center flex justify-center items-center py-2 px-4 rounded-md backdrop-opacity-5 backdrop-invert bg-white/60">
-        <div className="flex-1 text-5xl font-bold h-auto">25:00</div>
+        <h2 className="flex-1 text-5xl font-bold h-auto">
+          {String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
+        </h2>
         <div className="flex-1">
           <select name="setting" id="setting" className="mb-3 text-lg bg-transparent">
             <option value="5">5 min</option>
@@ -17,7 +53,12 @@ function HomePage(){
             <option value="45">45 min</option>
             <option value="60">60 min</option>
           </select>
-          <div className="py-1 bg-blue-600 text-white rounded-md">START</div>
+          <div 
+            className="py-1 bg-blue-600 text-white rounded-md"
+            onClick={handleClick}
+          >
+            {isRunning ? 'STOP' : 'START'}
+          </div>
         </div>
       </div>
     </div>
@@ -25,10 +66,6 @@ function HomePage(){
         .wrapper {
           width: 85vw;
           max-width: 350px;
-          /* border: solid 1px black; */
-        }
-        .visualTimer {
-          background: conic-gradient(#eaeaea 195deg, #061298 195deg 18deg);
         }
       `}</style>
     </>
