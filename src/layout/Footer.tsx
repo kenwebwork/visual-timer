7bedb3403f25226902e34d05bffc5dc3af5b492e
ken <kenwebworkwatarai@gmail.com>
2025-01-01 22:00:02 +0900

    add meta description

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,7 +2,9 @@ import { APP_NAME } from "../utils/constants";
 
 function Footer(){
   return (
-    <p className="max-w-96 m-auto pt-5 pb-1 text-sm text-center">{`Copyright © ${APP_NAME} 2025`}</p>
+    <p className="max-w-96 m-auto pt-5 pb-1 text-sm text-center text-gray-600">
+      {`Copyright © ${APP_NAME} 2025`}
+    </p>
   );
 };
 
--- src/layout/Title.tsx
+++ src/layout/Title.tsx
@@ -5,6 +5,7 @@ function Title(porps: {pageName:string}){
   return (
     <Helmet>
       <title>{porps.pageName} | {APP_NAME}</title>
+      <meta name="description" content="B-Timer is easy-to-read timer that is inspired Pomodoro Technique. 25 minutes work and 5 minutes break is one of the schedule we recommend. We’re rooting for you—let B-Timer boost your productivity and keep you in the zone!" />
     </Helmet>
   );
 };
